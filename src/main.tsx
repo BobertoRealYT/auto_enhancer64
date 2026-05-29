fe10b9599fde16098452bc6da7ee015e4510bf97
BobertoRealYT <BobertoRealYT@yahoo.com>
2026-05-29 22:07:11 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles.css'
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/auto_enhancer64/sw.js')
-  })
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
